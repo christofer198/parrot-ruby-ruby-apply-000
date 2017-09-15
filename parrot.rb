8de421fcad303cf0dc3e01f2251589172ffb4f60
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-15 00:19:42 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(in="Squawk!")
+def parrot(inno="Squawk!")
 
-  puts in
-  return in
+  puts inno
+  return inno
 end
